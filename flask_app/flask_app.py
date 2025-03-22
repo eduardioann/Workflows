c9a25a8257511045ba1f8e25c5eb623e197fe9e4
eduardioann <eduard.ioann@yahoo.com>
2025-03-22 21:46:42 +0200

    Message change

--- flask_app/flask_app.py
+++ flask_app/flask_app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Good evening, sir! \n You are very welcomed to my page. \n Enjoy!"
+    return "Good evening, sir!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
