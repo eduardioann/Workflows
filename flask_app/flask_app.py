c0accf5cf6b55fc75e8c02182bf5732ff3fa51d1
eduardioann <eduard.ioann@yahoo.com>
2025-03-22 21:02:50 +0200

    Message modified

--- flask_app/flask_app.py
+++ flask_app/flask_app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Docker and GitHub Actions!"
+    return "Good evening, sir! \n You are very welcomed to my page. \n Enjoy!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
